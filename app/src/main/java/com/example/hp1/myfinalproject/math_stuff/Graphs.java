8af37cdf5cc53e54349608591291052f74f67bd9
marwansakas <marwansakas.2000@gmail.com>
2017-10-17 00:26:09 +0300

    navigation drawer not finished

--- app/src/main/java/com/example/hp1/myfinalproject/math_stuff/Graphs.java
+++ app/src/main/java/com/example/hp1/myfinalproject/math_stuff/Graphs.java
@@ -1,23 +1,46 @@
 package com.example.hp1.myfinalproject.math_stuff;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TableLayout;
 
 import com.example.hp1.myfinalproject.R;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-public class Graphs extends AppCompatActivity {
+public class Graphs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     GraphView graphView;
     LineGraphSeries<DataPoint> series1,series2;
     double x,plusY,minusY;
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this, drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         graphView=(GraphView)findViewById(R.id.GraphView);//initialize graphView
         x=-5.1;//set the first x
         series1=new LineGraphSeries<>();//initialize series1
@@ -32,5 +55,20 @@ public class Graphs extends AppCompatActivity {
         graphView.addSeries(series1);//to add series1 to graphView
         graphView.addSeries(series2);//to add series2 to graphView
 
+        NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        actionBarDrawerToggle.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return true;
     }
 }
